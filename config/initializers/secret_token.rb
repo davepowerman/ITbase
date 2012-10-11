e6a1ed1812431b9cbf931975b8e0ac8c39ec005e
Pete <petevonna@lycos.com>
2012-10-10 21:00:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itstop::Application.config.secret_token = 'ed687e158137f398ac4238ff4db74eb25abdf6eb1d095f2e4d1aa94a82576e6153ea4a2b5c54ed14524bbc72b964241f243c639a346f26fdd73356eda36efd4b'
